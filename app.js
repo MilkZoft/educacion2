cb2f08143fc335777bd6f57e0a0e552618ee9fe8
Cristina Rojas <telematicacristina@gmail.com>
2015-07-19 19:25:03 -0700

    Implementing handlebars

--- app.js
+++ app.js
@@ -1,35 +1,60 @@
+'use strict';
+
+// Loading dependencies
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+// Initializing express application
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Loading config
+var config = require('./lib/config');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+// Body parser
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Logger
+var logger = require('morgan');
+app.use(logger('dev'));
+
+// Cookie Parser
+var cookieParser = require('cookie-parser');
 app.use(cookieParser());
+
+// Layout setup
+var exphbs = require('express-handlebars');
+
+// Stylus setup
+var stylus = require('stylus');
+var nib = require('nib');
+
+// Handlebars setup
+app.engine(config().views.extension, exphbs({
+  extname: config().views.extension,
+  defaultLayout: config().views.layout,
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials'
+}));
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', '.hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// Routes
+var home = require('./routes/home');
+var users = require('./routes/users');
+
+app.use('/', home);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -37,24 +62,28 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-
-module.exports = app;
+// Export application or start the server
+if (!!module.parent) {
+  module.exports = app;
+} else {
+  app.listen(config().serverPort);
+}
